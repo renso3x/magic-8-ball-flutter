a0a8ab30a4d00e8869f789de6b6d497120e8e758
Romeo <romeo.enso93@gmail.com>
2019-11-12 10:29:47 +0800

    magic 8ball, setstate, uses images, and layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        title: Text("Ask me Anything"),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  onChangeNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+
+    print(ballNumber);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          onPressed: onChangeNumber,
+          child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
+  }
+}
